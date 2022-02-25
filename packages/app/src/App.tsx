058e25825a5ea4caa8a63e7e8af99453ee32e434
matiasmateu <matias_mateu@icloud.com>
2022-02-25 19:39:55 +0100

    feat(ui): add secondary button

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Button  from '@ignitus/ui/src/Button';
+import SecondaryButton  from '@ignitus/ui/src/SecondaryButton';
 
 export default function App() {
   return (
@@ -9,6 +10,7 @@ export default function App() {
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
       <Button />
+      <SecondaryButton />
     </View>
   );
 }
